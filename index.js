f413056ef38e71e29f67de0f4d4e3997dc1d2033
Caroline <ckmcdon@gmail.com>
2022-05-18 16:23:42 -0400

    commit tree fe5b182a6128bb76f6d8d7169afcdf316221edbf

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function(el) {
+        return el.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(el) {
+        return el.startsWith(string)
+    })
+}
+
+function matchName(array, str) {
+    return array.filter( el => el.name === str ) 
+}
